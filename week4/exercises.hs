81bf1934b78195a5435391c918aac5780f4ce7ba
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2014-08-22 12:33:23 +0200

    Complete week 4 exercises (to review and refactor)

--- week4/exercises.hs
+++ week4/exercises.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wall #-}
+
 -- ---------- Exercise 1
 -- Exercise 1.1
 fun1 :: [Integer] -> Integer
@@ -19,3 +21,57 @@ fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (>1) . iterate (\x -> if even x 
                                                             then (x `div` 2) 
                                                             else (3 * x + 1))
+
+-- ---------- Exercise 2
+data Tree a = Leaf 
+            | Node Integer (Tree a) a (Tree a)
+              deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr addNode Leaf 
+
+addNode :: a -> Tree a -> Tree a
+addNode e Leaf = Node 0 Leaf e Leaf
+addNode e (Node 0 Leaf v Leaf) = Node 1 (addNode e Leaf) v Leaf
+addNode e (Node h l v Leaf) = Node h l v (addNode e Leaf)
+addNode e (Node h Leaf v r) = Node h (addNode e Leaf) v r
+addNode e (Node h l@(Node hl _ _ _) v r@(Node hr _ _ _))
+    | hl <= hr = case (addNode e l) of 
+                   (Node hl' _ _ _) -> Node (if hl' > hr then h + 1 else h) (addNode e l) v r
+    | otherwise = case (addNode e r) of
+                    (Node hr' _ _ _) -> Node (if hr' > hl then h + 1 else h) l v (addNode e r)
+
+-- foldTree "ABCDEFGHIJ" ==
+--          Node 3
+--                   (Node 2
+--                             (Node 0 Leaf ’F’ Leaf)
+--                             ’I’
+--                             (Node 1 (Node 0 Leaf ’B’ Leaf) ’C’ Leaf))
+--                   ’J’
+--                   (Node 2
+--                             (Node 1 (Node 0 Leaf ’A’ Leaf) ’G’ Leaf)
+--                             ’H’
+--                             (Node 1 (Node 0 Leaf ’D’ Leaf) ’E’ Leaf))
+                  
+
+-- ---------- Exercise 3.1
+xor :: [Bool] -> Bool
+xor = odd . foldl (\c x -> if x then c+1 else c) 0  
+
+-- ---------- Exercise 3.2
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\a bs -> [(f a)] ++ bs) [] 
+
+-- ---------- Exercise 3.3
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr (flip f) base (reverse xs) 
+
+-- ---------- Exercise 4
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram = map (\x -> 2*x+1) . (\xs -> filter (\x -> null (calcSolutions x (last xs))) xs) . (\x -> [1..x])
+
+calcSolutions :: Integer -> Integer -> [(Integer, Integer)]
+calcSolutions x n = [(i,j) | (i,j) <- cartProd [1..n] [1..n], i<=j, i+j+2*i*j<=n, i+j+2*i*j==x]
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x, y) | x <- xs, y <- ys]
