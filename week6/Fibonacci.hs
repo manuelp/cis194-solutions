ed83d412db71a5d9f2903306e4e2010dbc89782b
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-01-24 00:38:38 +0100

    Week 6, exercise 2.

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -11,3 +11,17 @@ fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+--
+-- Exercise 2
+--
+fibs2 :: [Integer]
+fibs2 = foo [0,1]
+
+foo :: [Integer] -> [Integer]
+foo [x1,x2] = foo [x1,x2,x1+x2]
+foo xs
+    | length xs > 2 = take (length xs - 2) xs 
+                      ++ 
+                      foo (reverse . take 2 . reverse $ xs)
+    | otherwise = xs
