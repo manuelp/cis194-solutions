895871573d67e22edcfabc04f079de0a9c6f15b3
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-01-24 00:06:09 +0100

    Week 6, exercise 1.

---
+++ week6/Fibonacci.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall #-}
+module Fibonacci where
+
+--
+-- Exercise 1
+--
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
