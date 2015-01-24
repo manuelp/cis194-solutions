4b0444ec211d1105158bca3a2901267361206abe
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-01-24 01:25:20 +0100

    Week 6, part of exercise 5.

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -48,3 +48,12 @@ streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f x = Cons x (streamFromSeed f $ f x)
+
+--
+-- Exercise 5
+--
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons a as) (Cons b bs) = Cons a (Cons b (interleaveStreams as bs))
